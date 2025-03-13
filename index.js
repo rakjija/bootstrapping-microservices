835e0f1beb868c887343def799f188454b474956
minjai100111 <minjai100111@gmail.com>
2025-03-14 03:56:51 +0900

    feat: add video file and create GET /video

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const fs = require('node:fs');
+
+const app = express();
+const port = 3000;
+
+app.get('/video', (req, res) => {
+  const path = './videos/SampleVideo_1280x720_1mb.mp4';
+  fs.stat(path, (err, stats) => {
+    if (err) {
+      console.error('An error occurred');
+      console.error(err);
+      res.sendStatus(500);
+      return;
+    }
+
+    res.writeHead(200, {
+      'Content-Length': stats.size,
+      'Content-Type': 'video/mp4',
+    });
+
+    fs.createReadStream(path).pipe(res);
+  });
+  // res.send('Hello, world!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
